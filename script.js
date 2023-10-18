cea4d30c2301db9006e8f4df5ec0dc971f49b60f
Nilesh Keshari <kesharinilesh13@gmail.com>
2023-10-18 20:27:05 +0530

    created timer and score counter

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+var timer=60;
+var score=0;
+function makeBubble(){
 var bubbler = "";
 
-for(var i = 1; i <= 176; i++) {
+for(var i = 1; i <= 198; i++) {
     var rn = Math.floor(Math.random()*10);
     bubbler += `<div class="bubble">${rn}</div>`;
 }
 document.querySelector(".pbottom").innerHTML = bubbler;
+}
+
+function hit(){
+    var val=Math.floor(Math.random()*10);
+    document.querySelector("#hitval").innerHTML = val;
+}
+
+function runtimer(){
+    setInterval(function(){
+        if(timer>0){
+            timer--;
+        document.querySelector("#timerval").innerHTML=timer;}
+},1000);
+}
+
+function increaseScore(){
+    score=score+10;
+    document.querySelector("#scoreval").innerHTML=score;
+}
+
+
+
+
+hit();
+increaseScore();
+runtimer();
+makeBubble();
